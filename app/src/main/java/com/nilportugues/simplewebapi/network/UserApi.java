168487a478613641a148b02b49a86f0a9cb8e7cd
Nil Portugués Calderó <nilopc@gmail.com>
2016-07-03 16:37:32 +0200

    removed innecessary final statement

--- app/src/main/java/com/nilportugues/simplewebapi/network/UserApi.java
+++ app/src/main/java/com/nilportugues/simplewebapi/network/UserApi.java
@@ -11,7 +11,7 @@ import retrofit2.http.Query;
 
 public interface UserApi {
 
-    static final String API_URL = "http://jsonplaceholder.typicode.com/";
+    String API_URL = "http://jsonplaceholder.typicode.com/";
 
     @GET("users")
     Call<List<User>> getUsers();
